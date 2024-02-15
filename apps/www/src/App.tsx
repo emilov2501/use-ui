8583dc65456c089c22b16f1f4f82750e0b312ba8
Eldiiar <emilov2501@gmail.com>
2024-02-16 00:40:34 +0600

    chore(app): update app configs

--- apps/www/src/App.tsx
+++ apps/www/src/App.tsx
@@ -1,14 +1,28 @@
-import { useButton, useField } from "useui-ts";
+import { CSSTransition } from "react-transition-group";
+import { useButton, useField, useModal } from "useui-ts";
+import "useui-ts/dist/index.css";
 import "./App.css";
 
 function App() {
+  const [modal, { Modal, isActive }] = useModal();
+
   const [{ debouncedValue, ...fieldProps }, { Field }] = useField();
 
-  const [btnProps, { Button }] = useButton();
+  const [btnProps, { Button }] = useButton({
+    onClick: () => modal.open("hello", {}),
+  });
 
   return (
     <>
       <Field {...fieldProps} className="hello" />
+      <CSSTransition
+        in={isActive}
+        timeout={modal.timeout}
+        classNames="modal"
+        unmountOnExit
+      >
+        <Modal className="modal--animate" />
+      </CSSTransition>
       {debouncedValue}
       <Button {...btnProps}>hello</Button>
     </>
