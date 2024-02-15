6abe8b352031ec8687608b674100768d307e172b
Eldiiar Temir <emilov2501@gmail.com>
2024-02-15 19:46:07 +0600

    chore(app): change exampe app

--- apps/www/src/App.tsx
+++ apps/www/src/App.tsx
@@ -1,13 +1,24 @@
-import { useButton } from "useui-ts";
+import { useButton, useField } from "useui-ts";
 import "./App.css";
 
 function App() {
+  const [{ debouncedValue, ...fieldProps }, { Field }] = useField({
+    debounceDelay: 1000,
+    value: "lox",
+  });
+
   const [btnProps, { Button }] = useButton({
     className: "lox",
     variant: "default",
   });
 
-  return <Button {...btnProps}>hello</Button>;
+  return (
+    <>
+      <Field {...fieldProps} className="hello" />
+      {debouncedValue}
+      <Button {...btnProps}>hello</Button>
+    </>
+  );
 }
 
 export default App;
