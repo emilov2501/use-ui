0e4f5cf0c5559e393f4f4f46088405ba841cde77
Eldiiar <emilov2501@gmail.com>
2024-02-21 00:12:38 +0600

    chore(app): change esbuild
    create scripts, dev, prod

---
+++ packages/use-ui/scripts/build.prod.mjs
@@ -0,0 +1,40 @@
+import styleXPlugin from "@stylexjs/esbuild-plugin";
+import * as esbuild from "esbuild";
+import CssModulesPlugin from "esbuild-css-modules-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let plugins = [
+  CssModulesPlugin(),
+  styleXPlugin({
+    dev: false,
+    useRemForFontSize: true,
+    generatedCSSFileName: "dist/components/styles.css",
+  }),
+];
+
+// Плагин для алиасов
+
+await esbuild.build({
+  metafile: true,
+  entryPoints: {
+    "hooks/index": path.resolve(__dirname, "..", "hooks/index.ts"),
+    "components/index": path.resolve(__dirname, "..", "components/index.ts"),
+  },
+  outdir: "dist",
+  splitting: true,
+  bundle: true,
+  sourcemap: false,
+  format: "esm",
+  define: {
+    "process.env.NODE_ENV": "'production'",
+  },
+  minify: true,
+  treeShaking: true,
+  plugins,
+  external: ["react", "react-dom"],
+  tsconfig: path.resolve(__dirname, "..", "tsconfig.build.json"),
+});
