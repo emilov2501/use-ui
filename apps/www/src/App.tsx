13dca3cd97d9459a4bf1b210e01e1db9eef3e003
Eldiiar <emilov2501@gmail.com>
2024-02-21 18:04:06 +0600

    fix: fix config
    package json , app.tsx

--- apps/www/src/App.tsx
+++ apps/www/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
       toast({
         title: "Uh No! Something went wrong",
         description: "There was a problet with your request",
+        variant: "error",
       }),
   });
 
