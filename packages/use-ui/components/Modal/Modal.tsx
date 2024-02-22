15f8383c40c7386d3e483d9b6e79e8b0088d4824
Eldiiar <emilov2501@gmail.com>
2024-02-22 13:18:59 +0600

    fix(useModal): fix styles view

--- packages/use-ui/components/Modal/Modal.tsx
+++ packages/use-ui/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import { wait } from "@/lib/helpers";
 import cls from "classnames";
-import React, { HTMLProps, forwardRef, useRef } from "react";
+import { HTMLProps, createElement as create, forwardRef, useRef } from "react";
 import { useOnClickOutside } from "usehooks-ts";
 import storage, { DELAY } from "../../hooks/useModal/useModalStore";
 import style from "./modal.module.css";
@@ -25,22 +25,20 @@ const Modal = forwardRef(({ currentModal }: Props, nodeRef) => {
 
   useOnClickOutside(modalRef, handleClose);
 
-  return React.createElement(
+  return create(
     "div",
-    {
-      className: style.container,
-    },
-    React.createElement(
+    { className: style.container },
+    create(
       "div",
-      { ref: modalRef },
-      React.createElement(
+      { className: style.wrapper, ref: modalRef },
+      create(
         "div",
         {
           className: cls(style.modal, style[size]),
           ref: nodeRef,
         },
-        React.createElement("h3", null, modalName),
-        React.createElement("div", { className: "content" }, props.content)
+        create("h3", null, modalName),
+        create("div", { className: "content" }, props.content)
       )
     )
   );
