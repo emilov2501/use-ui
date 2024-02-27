87386826c6c1e000c981523de1c0d2287b0870b9
xsander <emilov2501@gmail.com>
2024-02-27 14:03:51 +0600

    feat(useHeading): add a feature

--- packages/use-ui/lib/helpers.ts
+++ packages/use-ui/lib/helpers.ts
@@ -3,7 +3,7 @@
  * @param {number} [delay] - The `delay` parameter in the `wait` function is an optional parameter of
  * type `number`. It represents the time in milliseconds that the function will wait before resolving
  * the promise. If no `delay` value is provided, the default value is `undefined`.
- * @returns A Promise is being returned.
+ * @returns A Promise is being returned
  */
 export const wait = (delay?: number) => {
   return new Promise(function (resolve) {
