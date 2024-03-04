b89d13aec669e2174a69e210112a54f785fa1e26
xsander <emilov2501@gmail.com>
2024-03-04 17:34:12 +0600

    feat(useSwitch): add classnames, disabled

--- packages/use-ui/lib/hooks/useSwitch/useSwitch.ts
+++ packages/use-ui/lib/hooks/useSwitch/useSwitch.ts
@@ -4,12 +4,13 @@ import { CommonTypes, ResultSwitchProps } from "../../interfaces";
 
 interface SwitchOptions extends CommonTypes.ComponentDefaultAttributes {
   name?: string | undefined;
+  disabled?: boolean | undefined;
   value?: boolean | undefined;
   onToggle?: (value: boolean) => void;
 }
 
 const useSwitch = (props: SwitchOptions = {}): ResultSwitchProps => {
-  const { value, id, className, onToggle } = props;
+  const { value, id, className, onToggle, disabled, name } = props;
   const [isChecked, setIsChecked] = useState<boolean>(value || false);
 
   const toggleSwitch = useCallback((): void => {
@@ -19,16 +20,19 @@ const useSwitch = (props: SwitchOptions = {}): ResultSwitchProps => {
 
   const _className = useMemo(
     (): string =>
-      cls(className, "Switch__control", {
-        [`Switch__control--checked`]: isChecked,
+      cls(className, "UI_switch", {
+        [`UI_switch--checked`]: isChecked,
+        [`UI_switch--disabled`]: disabled,
       }),
-    [isChecked, className]
+    [isChecked, className, disabled]
   );
 
   return {
     id,
+    name,
     type: "checkbox",
     checked: isChecked,
+    disabled,
     onChange: toggleSwitch,
     tabIndex: 0,
     className: _className,
